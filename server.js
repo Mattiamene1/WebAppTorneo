3db55148049e4dbfc441a51a64dac22e1a711b4b
Mattiamene1 <meneghinmattia1@gmail.com>
2022-03-10 15:35:58 +0100

    First Commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+app.use("/assets", express.static(path.resolve(__dirname, "frontend", "assets")));
+app.use("/css", express.static(path.resolve(__dirname, "frontend", "assets", "css")));
+app.use("/font", express.static(path.resolve(__dirname, "frontend", "assets", "font")));
+app.use("/images", express.static(path.resolve(__dirname, "frontend", "assets", "images")));
+app.use("/js", express.static(path.resolve(__dirname, "frontend", "assets", "js")));
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+});
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(process.env.PORT || 3000, () => console.log("Server running ...."));
+
