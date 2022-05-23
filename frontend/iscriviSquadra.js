f18f09c0a7acca75e18aabd7c2e17e17f870a585
Mattia <meneghinmattia1@gmail.com>
2022-05-23 17:17:38 +0200

    adding frontend iscrivi

---
+++ frontend/iscriviSquadra.js
@@ -0,0 +1,5 @@
+var numPlayers = document.getElementById("numPlayers");
+var indexSelected = numPlayers.selectedIndex;
+
+var valueSelected = numPlayers.options[indexSelected];
+var value = valueSelected.value;
