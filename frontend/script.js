f28118bf9ad39ab1cc028dfca5b0be36c1e204eb
Riccardo Gilli <gilli.riccardo@gmail.com>
2022-05-28 12:02:06 +0200

    Script to get login token, approvazione arbitri

--- frontend/script.js
+++ frontend/script.js
@@ -1,18 +1,13 @@
+const API_V1 = "http://localhost:3000/api/v1/"
+const API_V2 = "http://localhost:3000/api/v2/"
+let AUTH_TOKEN = null;
+
 function login() {
-    var email = document.getElementById("loginEmail").value;
-    var password = document.getElementById("loginPassword").value;
-    fetch('../api/v1/authentications', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify( { email: email, password: password } ),
-    }).then((resp) => resp.json()) // Transform the data into json
-    .then(function(data) { // Here you get the data to modify as you please
-    loggedUser.token = data.token;
-    loggedUser.email = data.email;
-    loggedUser.id = data.id;
-    loggedUser.self = data.self;
-    document.getElementById("loggedUser").innerHTML = loggedUser.email;
-    loadLendings();
-    return;
-    }).catch( error => console.error(error) );
+    let token_cookie = document.cookie.split('; ').find(row => row.startsWith('token='))
+    if (token_cookie) {
+        AUTH_TOKEN = token_cookie.split("=")[1];
+    } else {
+        alert("Questa pagina richiede un'autenticazione ma l'utente non risulta autenticato");
+    }
+    
 };
